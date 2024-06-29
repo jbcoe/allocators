ea562ac1028f7a23da0aca20a2654cc9cd8a986e
Jonathan B. Coe <jonathanbcoe@gmail.com>
2024-06-29 17:39:13 +0100

    Add some documentation to slide-generation script

--- scripts/slides.sh
+++ scripts/slides.sh
@@ -1,3 +1,12 @@
-!#/usr/bin/env bash
+#!/usr/bin/env bash
+
+# Install Marp CLI
+# https://formulae.brew.sh/formula/marp-cli
+
+
+# Generate slides
+# ./scripts/slides.sh
+
 cd $(dirname $(dirname "$0"))
+set -x
 marp TALK.md --allow-local-files --html -o TALK.pdf
